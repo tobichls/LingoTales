994d30e1e73812ca388fe191162c4336e97f8045
Janett Gerrard <mikanchik@gmail.com>
2024-03-10 10:16:32 +0000

    add a gradient

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/IMG-20240310-WA0002.jpg"
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="lingotales logo" id="logo" />
+    </header>
+  )
+}
+
+export default Header;
