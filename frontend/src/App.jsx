f3a1252037f95fedc830844a54cb2ee072a257e5
Janett Gerrard <mikanchik@gmail.com>
2024-03-10 11:08:39 +0000

    clean up code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,9 +24,6 @@ const myVoices = {
 }
 
 
-
-
-
 function App() {
   const [ options, setOptions ] = useState([])
   const [ message, setMessage ]  = useState("")
@@ -34,30 +31,6 @@ function App() {
   const [ panels, setPanels ] = useState([])
   const [ formSubmitted, setFormSubmitted ] = useState(false)
   const [ contextualData, setContextualData ] = useState(null)
-
-  // const start = (data) => {
-  //   fetch('http://127.0.0.1:5000/start', {
-  //         method: 'POST',
-  //         mode: 'cors',
-  //         headers: {
-  //             'Content-Type': 'application/json'
-  //         },
-  //         body: JSON.stringify(data)
-  //     })
-  //     .then(response => {
-  //         if (!response.ok) {
-  //             throw new Error('Network response was not ok');
-  //         }
-  //         return response.json();
-  //     })
-  //     .then(responseJson => {
-  //         setMessage(responseJson.scene)
-  //         setOptions(responseJson.option1, responseJson.option2, responseJson.option3)
-  //     })
-  //     .catch(error => {
-  //         console.error('There has been a problem with your fetch operation:', error);
-  //     });
-  // }
 
 
   const handleFormSubmittedState = (data) => {
@@ -121,17 +94,12 @@ function App() {
   }
 
 
-
-
-
+  // when the user chooses a story option,
+  // send the entire context and story so far to the LLM
   const next = (event) => {
     const selectedOption = event.target.id
-    console.log(selectedOption)
     const selectedText = event.target.innerHTML
-
-
-
-
+    // add current panel to stored panels
     const newPanels = [...panels, {
       "scene": nextPanels[selectedOption].scene,
       "option1": nextPanels[selectedOption].option1,
@@ -144,21 +112,12 @@ function App() {
     setOptions([nextPanels[selectedOption].option1, nextPanels[selectedOption].option2, nextPanels[selectedOption].option3])
     setPanels(newPanels)
 
-
-    // const newPanels = [...panels, {
-    //   "scene": message,
-    //   "option1": options[0],
-    //   "option2": options[1],
-    //   "option3": options[2],
-    //   "userChoice": selectedOption
-    // }]
-
     const data = {
       ...contextualData,
       "panels": newPanels,
     }
-    console.log(data)
 
+    // call backend API
     fetch('http://127.0.0.1:5000/action', {
         method: 'POST',
         mode: 'cors',
@@ -181,9 +140,10 @@ function App() {
     });
   }
 
+
+
+  // when data is returned, update the state and UI
   const update = (data) => {
-    // setMessage(data.scene)
-    // setOptions([data.option1, data.option2, data.option3])
     console.log("update: ")
     console.log(data)
     setNextPanels(data.scenes)
@@ -230,28 +190,6 @@ function App() {
   var timeout_id = null
 
 
-
-  // const toggleAnim = (i, option) => {
-
-  //   var btn = document.getElementById(String(i))
-  //   if(btn){
-  //     btn.classList.toggle("hover-color-shift")
-  //   }
-
-  //   if(!hovering){
-  //     timeout_id = setTimeout(() => {
-  //       translateAndSpeak(option)
-  //     }, 3000);
-  //     hovering = true
-  //   }else{
-  //     hovering = false
-  //     clearTimeout(timeout_id);
-  //     timeout_id = null;
-  //   }
-
-
-  // }
-
   return (
     <>
       <Header></Header>
@@ -263,38 +201,21 @@ function App() {
                <div className="loading-indicator">Loading...</div>
              ) : (
               options.map((option, i) => {
-                // return AnimatedButton(option, () => {}, next, i, "blue", "red")
 
                 return (
                   <button
                     className={`custom-animated-button`}
-                    // style={{ backgroundColor: "blue"}}
-                    // onMouseDown={handleMouseEnter(option, i)}
-                    // onMouseUp={handleMouseLeave(i)}
                     onContextMenu={(event) => {
                       event.preventDefault();
                       // toggleAnim(i, option);
                     }}
                     id={i}
+                    key={i}
                     onClick={next}
                   >
                     {option}
                   </button>
                 );
-
-
-                // (
-                //   <button
-                //     key={i}
-                //     className="option"
-                //     onClick={next}
-                //     id={i}
-                //     onMouseEnter={() => translateAndSpeak(option)}
-                //   >
-                //     {option}handleButtonClick
-                //   </button>
-                // )
-
 
               })
             )}
